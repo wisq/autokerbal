598324805991583f5096569383cf705b0c4965fa
Adrian Irving-Beer <wisq@wisq.net>
2016-09-19 16:24:04 -0400

    Update explorer.

--- explorer.rb
+++ explorer.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby
 
-require 'bundler/setup'
-require 'krpc'
-require 'pry'
+$LOAD_PATH << File.join(File.dirname($0), 'lib')
+require 'kerbal'
+require 'matrix' # for Vector
 
-PROGRAM = File.basename($0, ".rb")
-@client  = KRPC.connect(name: PROGRAM, host: "omgwtfhax.wisq.org")
-@space_center = @client.space_center
-@vessel  = @space_center.active_vessel
-@control = @vessel.control
-pry
+Kerbal.execute 'explorer' do
+  binding.pry
+end
