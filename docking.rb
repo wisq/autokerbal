f942c209c85a65100a387ef9aedc59182f6ce523
Adrian Irving-Beer <wisq@wisq.net>
2016-10-04 23:11:10 -0400

    Heavily rework docking script to use better reference frames.

--- docking.rb
+++ docking.rb
@@ -10,25 +10,36 @@ MAXIMUM_ERROR = 0.2   # metres of lateral error
 Kerbal.thread 'docking' do
   target = @space_center.target_docking_port
   raise "no target docking port" if target.nil?
-
-  puts "Target: #{target.part.title} on #{target.part.vessel.name}"
   our_port = @vessel.parts.controlling.docking_port
+  raise "no controlling docking port" if our_port.nil?
+
+  target_vessel = target.part.vessel
+  puts "Target: #{target.part.title} on #{target_vessel.name}"
+  puts "Controlling from: #{our_port.part.title}"
 
   puts "Aligning with docking port ..."
+  # For some reason, I can't align directly to the target reference frame.
+  # So I'll align to the ship instead.
+  alignment = @space_center.transform_direction(
+    direction: [0, -1, 0],
+    from: target.reference_frame,
+    to: target_vessel.reference_frame,
+  )
+
   @control.sas = false
-  @autopilot.reference_frame = target.reference_frame
-  @autopilot.target_direction = [0, -1, 0]
+  @autopilot.reference_frame = target_vessel.reference_frame
+  @autopilot.target_direction = alignment
   @autopilot.target_roll = 0
   @autopilot.engage
   sleep(1)
   @autopilot.wait
 
-  @control.rcs = true
   closing_stage = false
 
   until target.state == :docking do
-    position = Vector[*our_port.position(target.reference_frame)]
-    velocity = Vector[*@vessel.velocity(target.reference_frame)]
+    @control.rcs = true
+    position = Vector[*target.position(our_port.reference_frame)]
+    velocity = Vector[*target_vessel.velocity(our_port.reference_frame)]
     on_target = true
 
     # FIXME: man this is gross
@@ -37,47 +48,57 @@ Kerbal.thread 'docking' do
     #  * translate vessel velocity to docking port velocity
     #  * use correct up/right/forward based on translated velocity
 
-    y_delta = position[0]
-    y_speed = velocity[0]
-    if y_delta.abs <= MAXIMUM_ERROR
-      change_y_speed(0.0, y_speed)
-    else
-      on_target = false
-      target_speed = -y_delta / 20.0
-      target_speed =  1 if target_speed >  1
-      target_speed = -1 if target_speed < -1
-      change_y_speed(target_speed, y_speed)
-    end
-
-    x_delta = position[2]
-    x_speed = velocity[2]
+    x_delta = position[0]
+    x_speed = -velocity[0]
     if x_delta.abs <= MAXIMUM_ERROR
       change_x_speed(0.0, x_speed)
     else
       on_target = false
-      target_speed = -x_delta / 20.0
+      target_speed = x_delta / 5.0
       target_speed =  1 if target_speed >  1
       target_speed = -1 if target_speed < -1
       change_x_speed(target_speed, x_speed)
     end
 
-    closing_stage = true if on_target
-
-    z_delta = position[1]
-    z_speed = velocity[1]
-    if closing_stage
-      target_speed = -z_delta / 20.0
-      change_z_speed(target_speed, z_speed)
+    y_delta = position[2]
+    y_speed = -velocity[2]
+    if y_delta.abs <= MAXIMUM_ERROR
+      change_y_speed(0.0, y_speed)
     else
-      change_z_speed(0.0, z_speed)
+      on_target = false
+      target_speed = y_delta / 5.0
+      target_speed =  1 if target_speed >  1
+      target_speed = -1 if target_speed < -1
+      change_y_speed(target_speed, y_speed)
     end
 
-    p [x_delta, y_delta, z_delta, on_target]
+    closing_stage = true if on_target
+
+    closing_delta = position[1]
+    closing_speed = -velocity[1]
+
+    unless closing_stage
+      # Target a point 50m from their docking port
+      closing_delta -= 50
+    end
+
+    target_speed = closing_delta / 10.0
+    target_speed =  1 if target_speed >  1
+    target_speed = -1 if target_speed < -1
+
+    if closing_stage
+      # always push closer
+      target_speed = 0.1 if target_speed < 0.1
+    end
+    change_closing_speed(target_speed, closing_speed)
+
+    p [x_delta, y_delta, closing_delta, on_target]
     sleep(0.1)
   end
 
   @control.up = 0.0
   @control.right = 0.0
+  @control.forward = 0.0
   @control.rcs = false
 
   puts "In range of docking port, waiting for docking."
@@ -103,7 +124,7 @@ def change_y_speed(target_speed, current_speed)
     delta = -MINIMUM_THRUST if delta > -MINIMUM_THRUST && delta > MINIMUM_DELTA
   end
   p ['change_y', target_speed, current_speed, delta]
-  @control.up = delta
+  @control.up = -delta
 end
 
 def change_x_speed(target_speed, current_speed)
@@ -114,11 +135,10 @@ def change_x_speed(target_speed, current_speed)
     delta = -MINIMUM_THRUST if delta > -MINIMUM_THRUST && delta > MINIMUM_DELTA
   end
   p ['change_x', target_speed, current_speed, delta]
-  @control.right = -delta
+  @control.right = delta
 end
 
-def change_z_speed(target_speed, current_speed)
-  p ['change_z', target_speed, current_speed]
+def change_closing_speed(target_speed, current_speed)
   delta = (target_speed - current_speed) * 2
   if delta.positive?
     delta = MINIMUM_THRUST if delta < MINIMUM_THRUST && delta > MINIMUM_DELTA
@@ -126,7 +146,7 @@ def change_z_speed(target_speed, current_speed)
     delta = -MINIMUM_THRUST if delta > -MINIMUM_THRUST && delta > MINIMUM_DELTA
   end
   p ['change_z', target_speed, current_speed, delta]
-  @control.forward = -delta
+  @control.forward = delta
 end
 
 Kerbal.run
